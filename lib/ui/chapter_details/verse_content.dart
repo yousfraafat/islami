4ff474a784abee259a4f15120b93526ac0adec8e
yousf <yousfraafat888@gmail.com>
2024-08-27 03:41:13 +0300

    feat: added chapter details screen

---
+++ lib/ui/chapter_details/verse_content.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class VerseContent extends StatelessWidget {
+  String content;
+  int index;
+
+  VerseContent(this.content, this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '$content (${index + 1})',
+      textDirection: TextDirection.rtl,
+      textAlign: TextAlign.center,
+      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+    );
+  }
+}
