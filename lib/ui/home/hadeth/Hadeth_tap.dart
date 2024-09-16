41453c984a5a4c398e28e7ade199743d878ec05f
yousf <yousfraafat888@gmail.com>
2024-09-16 23:35:46 +0300

    feat: apply localization

--- lib/ui/home/hadeth/Hadeth_tap.dart
+++ lib/ui/home/hadeth/Hadeth_tap.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:islami2/ui/home/hadeth/hadeth_title.dart';
 
 import '../../theme/mytheme.dart';
+import '../../ui_utills.dart';
 
 class HadethTap extends StatefulWidget {
   HadethTap({super.key});
@@ -31,7 +32,7 @@ class _HadethTapState extends State<HadethTap> {
           height: 2,
           color: MytThemeData.lightPrimary,
         ),
-        Text('Hadeth',
+        Text(AppTranslations(context).hadethNum,
             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
         Container(
           width: double.infinity,
--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:islami2/ui/home/hadeth/Hadeth_tap.dart';
 import 'package:islami2/ui/home/quran/Quran_tap.dart';
 import 'package:islami2/ui/home/radio/Radio_tap.dart';
 import 'package:islami2/ui/home/tasbeh/tasbeh_tap.dart';
+import 'package:islami2/ui/ui_utills.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String routeName = 'Home';
@@ -25,7 +26,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return DefaultScreen(
       body: Scaffold(
         appBar: AppBar(
-          title: Text('Islami'),
+          title: Text(AppTranslations(context).appTitle),
         ),
         body: taps[selectedIndex],
         bottomNavigationBar: BottomNavigationBar(
@@ -36,10 +37,14 @@ class _HomeScreenState extends State<HomeScreen> {
           },
           currentIndex: selectedIndex,
           items: [
-            BottomNavItem('assets/images/icon_quran.png', 'quran'),
-            BottomNavItem('assets/images/icon_hadeth.png', 'hadeth'),
-            BottomNavItem('assets/images/icon_sebha.png', 'tasbeh'),
-            BottomNavItem('assets/images/icon_radio.png', 'radio'),
+            BottomNavItem('assets/images/icon_quran.png',
+                AppTranslations(context).quranTap),
+            BottomNavItem('assets/images/icon_hadeth.png',
+                AppTranslations(context).hadethTap),
+            BottomNavItem('assets/images/icon_sebha.png',
+                AppTranslations(context).tasbehTap),
+            BottomNavItem('assets/images/icon_radio.png',
+                AppTranslations(context).radioTap),
           ],
         ),
       ),
